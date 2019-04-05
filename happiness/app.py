983d193ea134a9cc7f0faf013b89314167eb71d5
bdthai81 <bdthai81@gmail.com>
2019-04-04 23:53:00 -0700

    Updated index.html template design with Cossela. Setup template html pages for scatter, geomap, and radar html.

--- happiness/app.py
+++ happiness/app.py
@@ -13,5 +13,26 @@ def index():
     # return render_template("index.html", happiness=happiness_data)
     return render_template("index.html")
 
+# leaflet geomap: 
+@app.route("/geomap")
+def geomap():
+    # happiness_data = mongo.db.happiness.find_one()
+    # return render_template("index.html", happiness=happiness_data)
+    return render_template("geomap.html")
+
+# radar chart: by year with all other columns
+@app.route("/radar")
+def radar():
+    # happiness_data = mongo.db.happiness.find_one()
+    # return render_template("index.html", happiness=happiness_data)
+    return render_template("radar.html")
+
+# scatter chart: by year with all other columns 
+@app.route("/scatter")
+def scatter():
+    # happiness_data = mongo.db.happiness.find_one()
+    # return render_template("index.html", happiness=happiness_data)
+    return render_template("scatter.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ happiness/static/js/app.js
@@ -0,0 +1,68 @@
+/**
+ * D3 Add cards for each column of data covered in Happiness
+ */
+
+ // Will add this to be loaded from API
+var happinessData = [
+    {
+        title: "Suicide",
+        desc: "blah blah"
+    },
+    {
+        title: "GPI Score",
+        desc: "blah blah"
+    },
+    {
+        title: "Life Ladder",
+        desc: "blah blah"
+    },
+    {
+        title: "GDP per Capita",
+        desc: "blah blah"
+    },
+    {
+        title: "Freedom",
+        desc: "Freedom to make life choices"
+    },
+    {
+        title: "Generosity",
+        desc: "blah blah"
+    },
+    {
+        title: "Confidence",
+        desc: "Confidence in national government"
+    },
+    {
+        title: "Years",
+        desc: "blah blah"
+    }
+];
+
+/**
+ * Create the card for each happniessData
+ */
+var factors = d3.select("#factors");
+happinessData.forEach( e => {
+    // Append Card
+     var card = factors.append("div").classed("col-xs-12 col-md-3", true)
+        .append("div").classed("card border-danger mb-3", true).attr("style", "max-width: 20rem; min-height: 15rem;")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
+    card.append("div").classed("card-header", true).text(`${e.title}`);
+    card.append("div").classed("card-body", true)
+        .append("p").classed("card-text", true).text(e.desc);
+        
+});
+
+/**
+ * function hover over and out: change the card background colors
+ */
+function handleMouseOver()
+{
+    d3.select(this).classed("text-white bg-warning", true);
+    d3.select(this).classed("border-danger", false);
+};
+function handleMouseOut() {
+    d3.select(this).classed("border-danger", true);
+    d3.select(this).classed("text-white bg-warning", false);
+};

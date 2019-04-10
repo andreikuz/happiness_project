671238134f7efa7dbf11b7318bf7ec925afe3f89
Cossella <cossella@gmail.com>
2019-04-09 21:14:13 -0700

    updated labels

--- happiness/factors.py
+++ happiness/factors.py
@@ -3,17 +3,17 @@
 factors = [
     {"title": "Suicide",
     "desc": "measured by the number of suicides in each country"},
-    {"title": "Global Peace Index (GPI)", 
+    {"title": "Global Peace Index", 
     "desc": "measured by the relative position of nations' and regions' level of peacefulness"},
-    {"title": "Life Ladder", 
+    {"title": "Happiness", 
     "desc": "measured by satisfaction and happiness of life"},
-    {"title": "Gross Domestic Product (GDP)", 
+    {"title": "Gross Domestic Product", 
     "desc": "measured by Purchasing Power Parity per capita"},
-    {"title": "Freedom to Make Life Choices", 
-    "desc": "measured by the satisfaction of freedom to choose what to do with one's life"},
+    {"title": "Freedom", 
+    "desc": "measured by the freedom to make life choices"},
     {"title": "Generosity", 
     "desc": "measured by recent donations to charity"},
-    {"title": "Trust in the National Government", 
+    {"title": "Trust in Government", 
     "desc": "measured by a perceived absence of corruption in government and business"},
     {"title": "Years",
     "desc": "2011, 2012, 2013, 2014, 2015, 2016"}
@@ -23,3 +23,4 @@ factors = [
 
 def factors_data():
     return factors
+

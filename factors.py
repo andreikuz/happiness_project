7b7611738ecfa44f1d3f6683add9debdb9691707
bdthai81 <bdthai81@gmail.com>
2019-04-12 00:41:21 -0700

    make it better

--- factors.py
+++ factors.py
@@ -2,14 +2,14 @@
 
 # Tuples of (title, desc)
 factors = [
-    ("Suicide Rate", "measured by the number of suicides divided by population per 100k in each country",),
-    ("Global Peace Index", "measured by the relative position of nations' and regions' level of peacefulness",),
-    ("Happiness", "measured by satisfaction and happiness of life",),
-    ("Gross Domestic Product", "measured by Purchasing Power Parity per capita",),
-    ("Freedom", "measured by the freedom to make life choices",),
-    ("Generosity", "measured by recent donations to charity",),
-    ("Trust in Government", "measured by a perceived absence of corruption in government and business",),
-    ("Years", "2011, 2012, 2013, 2014, 2015, 2016",)
+    ("Suicide Rate", "measured by the number of suicides divided by population per 100k in each country"),
+    ("Global Peace Index", "measured by the relative position of nations' and regions' level of peacefulness"),
+    ("Happiness", "measured by satisfaction and happiness of life"),
+    ("Gross Domestic Product", "measured by Purchasing Power Parity per capita"),
+    ("Freedom", "measured by the freedom to make life choices"),
+    ("Generosity", "measured by recent donations to charity"),
+    ("Trust in Government", "measured by a perceived absence of corruption in government and business"),
+    ("Years", "2011, 2012, 2013, 2014, 2015, 2016")
 ]
 
 # Define function to pass back the factors data
